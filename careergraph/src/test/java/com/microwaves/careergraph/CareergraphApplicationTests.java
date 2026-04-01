7b4b8f0abbbe8046f866f6fe9a5578c46ae3fbcc
jvvidalb <joao.victorvidalb@gmail.com>
2026-04-01 10:12:26 -0300

    Backend simple test setup

--- careergraph/src/test/java/com/microwaves/careergraph/CareergraphApplicationTests.java
+++ careergraph/src/test/java/com/microwaves/careergraph/CareergraphApplicationTests.java
@@ -2,12 +2,14 @@ package com.microwaves.careergraph;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CareergraphApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true, "The application context should load successfully");
 	}
 
 }
